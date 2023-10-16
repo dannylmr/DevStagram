90ccd2aaaebc045cf5633a7b6786cb97d23463cb
Danny Martinez <dannylmr@gmail.com>
2023-10-16 11:23:39 -0500

    Se crean controlladores,vistas de inicio de sesion, logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,32 @@
                 DevStagram
             </h1>
 
-            <nav class="flex-2 items-center">
-                <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
-            </nav>
+            @auth()
+                <nav class="flex-2 items-center">
+                    <a class="font-bold text-gray-600 text-sm" href="#">
+                        Hola:
+                        <span class=" font-normal">
+                            {{ auth()->user()->username }}
+                        </span>
+                    </a>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="font-bold uppercase text-gray-600 text-sm">
+                            Cerrar Sesion
+                        </button>
+                    </form>
+                </nav>
+            @endauth
+
+            @guest
+                <nav class="flex-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
+                </nav>
+            @endguest
+
+
         </div>
 
     </header>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
@@ -20,7 +22,12 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/register', [RegisterController::class, 'store'])->name('register');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/wall', [PostController::class, 'index'])->name('posts.index');
 
